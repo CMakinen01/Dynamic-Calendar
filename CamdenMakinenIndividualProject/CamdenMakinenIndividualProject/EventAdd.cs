5028ef0bbd8f9f46659779aa0ae774026c1877e1
Camden Makinen <camden.makinen@gmail.com>
2024-10-28 18:05:35 -0400

    Test Git push

--- CamdenMakinenIndividualProject/CamdenMakinenIndividualProject/EventAdd.cs
+++ CamdenMakinenIndividualProject/CamdenMakinenIndividualProject/EventAdd.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-
+//Test Git Repo
 namespace CamdenMakinenIndividualProject
 {
     public partial class EventAdd : Form
